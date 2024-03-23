46cd61a1e0768e60f9345ee482b41aee7af8d4cf
YadavAnuj90 <163463130+YadavAnuj90@users.noreply.github.com>
2024-03-23 20:01:29 +0530

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -26,62 +26,62 @@
  greet(`anuj`)
  **/
 
-//  function findEvenOrOdd(num){
+ function findEvenOrOdd(num){
 
-//     if(num%2 ==0){
+    if(num%2 ==0){
 
-//         return "Even";
-//     }else{
-//         return "Odd";
-//     }
+        return "Even";
+    }else{
+        return "Odd";
+    }
 
-//  }
-//  console.log(findEvenOrOdd(23));
-//  console.log(findEvenOrOdd(22));
+ }
+ console.log(findEvenOrOdd(23));
+ console.log(findEvenOrOdd(22));
 
-//convert to arrow function
+convert to arrow function
 
-//  myfunc = (num)=>{
+ myfunc = (num)=>{
 
-//     if(num%2==0){
-//         return "Even"
-//     }else{
-//         return "odd"
-//     }
-//  }
-//   console.log(myfunc(23));
+    if(num%2==0){
+        return "Even"
+    }else{
+        return "odd"
+    }
+ }
+  console.log(myfunc(23));
 
-//convert in single line
+convert in single line
 
-//   fun =(a) => (a%2==0)?"even":"odd"
+  fun =(a) => (a%2==0)?"even":"odd"
 
-//   console.log(fun(22));
+  console.log(fun(22));
 
-// const user = {
-//   username: "Anuj",
-//   price: 999,
+const user = {
+  username: "Anuj",
+  price: 999,
 
-//   welcomeMessage: function () {
-//     console.log(`${this.username},welcome to website`);
-//     console.log(this);
-//   },
-// };
-//  user.welcomeMessage()
-// user.username = "Sam"
-// console.log(this);
-
-
-// const addSum = (a,b) =>{
-//       return a+b
-// }
-// console.log(addSum(5,4));
+  welcomeMessage: function () {
+    console.log(`${this.username},welcome to website`);
+    console.log(this);
+  },
+};
+ user.welcomeMessage()
+user.username = "Sam"
+console.log(this);
 
 
+const addSum = (a,b) =>{
+      return a+b
+}
+console.log(addSum(5,4));
 
- // emplicit function 
-// const addTwo = (num1, num2) => (num1*num2)
 
-// console.log(addTwo(2,3));
+
+ emplicit function 
+const addTwo = (num1, num2) => (num1*num2)
+
+console.log(addTwo(2,3));
 
 function connect() 
 {
